fe87db79e369c795f6a9f803d3865f1471c0e312
Michael Liu <michael.liu.mike@gmail.com>
2023-06-27 14:01:44 -0700

    inital homepage setup

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,8 +2,42 @@ import React, { useState, FC } from 'react';
 
 const HomePage: FC = () => {
   const [username, setUsername] = useState<string>("");
+  const [gameCode, setGameCode] = useState<string>("");
 
-  return <div>HomePage</div>;
+  
+  return (
+    <>
+      <h1>hompage</h1>
+      <input 
+        type="text" 
+        value={username} 
+        onChange={(e) => {
+          setUsername(e.target.value)
+          console.log(username)
+        }}
+        placeholder="Ash kethcin"
+      />
+      <input 
+        type="text" 
+        value={gameCode} 
+        onChange={(e) => setGameCode(e.target.value)}
+        placeholder="public game code"
+      />
+      <button
+        onClick={() => {}} //fill in later
+        disabled={username === "" || gameCode !== ""}
+      >
+        Create Game
+      </button>
+      <button
+        onClick={() => {}} //fill in later
+        disabled={username === "" || gameCode === ""}
+      >
+        Join game 
+      </button> 
+    </>
+
+  );
 };
 
 export default HomePage;
