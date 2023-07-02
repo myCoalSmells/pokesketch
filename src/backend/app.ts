23203238ba1a2203f890338e9072a095621d89af
Christen <99223047+christen03@users.noreply.github.com>
2023-07-02 00:36:16 -0700

    new hashmaps lol

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -15,28 +15,43 @@ const io = new Server(server, {
   },
 });
 
-const players: { [key: string]: { username: string, room: string } } = {}; // all players
+interface UserData {
+  username: string,
+  socketId: string
+}
+
+// Map to store all player data currently playing
+// Game Room => [{username, user's socketID}]
+const allPlayers = new Map<string, UserData[]>();
+const socketIdToGameCode = new Map<string, string>();
 
 io.on('connection', (socket: any) => {
   console.log(`${socket.id} connected`);
-
   socket.on('join_room', ({ username, gameCode }: any) => {
     console.log('User', username, 'joined room', gameCode);
     console.log(socket.id);
     socket.join(gameCode);
-    players[socket.id] = { username, room: gameCode }; // add new player to list
-
-    const playersInRoom = Object.values(players).filter((player) => player.room === gameCode);
-    io.to(gameCode).emit('players_in_room', playersInRoom); // send users in room to all clients in room
+    socketIdToGameCode.set(socket.id, gameCode);
+    const newUser : UserData = { username, socketId: socket.id };
+    // If doesn't exist, create new room, but tell user it was created
+    const players = allPlayers.get(gameCode) || [];
+    players.push(newUser);
+    allPlayers.set(gameCode, players); // add new player to list
+    io.to(gameCode).emit('players_in_room', players); // send users in room to all clients in room
   });
 
   socket.on('disconnect', () => {
     console.log(`user disconnected: ${socket.id}`);
-    const { room } = players[socket.id] || {};
-    if (room) {
-      delete players[socket.id]; // remove player if disconnect
-      const playersInRoom = Object.values(players).filter((user) => user.room === room);
-      io.to(room).emit('players_in_room', playersInRoom);
+    const gameCode = socketIdToGameCode.get(socket.id);
+    if (gameCode) {
+      socketIdToGameCode.delete(socket.id);
+      const newPlayers = allPlayers.get(gameCode);
+      if (newPlayers) {
+        const index = newPlayers.findIndex((player) => player.socketId === socket.id);
+        newPlayers.splice(index, 1);
+        allPlayers.set(gameCode, newPlayers);
+        io.to(gameCode).emit('players_in_room', newPlayers);
+      }
     }
   });
 });
